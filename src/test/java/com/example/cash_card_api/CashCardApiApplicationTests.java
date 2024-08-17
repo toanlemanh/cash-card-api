b7601e57f91af6921d1d0dfb158456fda17d21b7
toanlemanh <toanlemanh2003@gmail.com>
2024-08-17 14:46:33 +0700

    config(build.gradle): disable spring security

--- src/test/java/com/example/cash_card_api/CashCardApiApplicationTests.java
+++ src/test/java/com/example/cash_card_api/CashCardApiApplicationTests.java
@@ -1,14 +1,16 @@
 package com.example.cash_card_api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
 class CashCardApiApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
+
+
 
 
 }
